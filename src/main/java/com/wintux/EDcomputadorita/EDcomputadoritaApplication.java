15e9cd3c1a7fd1cf6078ffa02d84c1c1afb120ac
Stephanie Lazarte <lazarterodasstephanie@gmail.com>
2026-05-29 14:05:17 -0400

    Primer commit

---
+++ src/main/java/com/wintux/EDcomputadorita/EDcomputadoritaApplication.java
@@ -0,0 +1,13 @@
+package com.wintux.EDcomputadorita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDcomputadoritaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDcomputadoritaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wintux/EDcomputadorita/EDcomputadoritaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wintux.EDcomputadorita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDcomputadoritaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
